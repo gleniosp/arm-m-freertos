67dc53a12eba020a8c4da152c0240b69acbc478f
Glênio Silva Pimentel <11273108+gleniosp@users.noreply.github.com>
2026-02-10 10:41:16 +0000

    18 Correcting the boot-up code and memory init

--- port/start.c
+++ port/start.c
@@ -19,9 +19,30 @@ static void vTask2(void *pvParameters)
     };
 }
 
+extern uint32_t _sdata;  // Start of .data in RAM
+extern uint32_t _edata;  // End of .data in RAM
+extern uint32_t _sidata; // Start of .data in Flash
+extern uint32_t _sbss;   // Start of .bss
+extern uint32_t _ebss;   // End of .bss
+
 void start()
 {
     BaseType_t xReturn;
+
+    // Initialize .data section (copy from Flash to RAM)
+    uint32_t *src = &_sidata; // Source (Flash)
+    uint32_t *dst = &_sdata;  // Destination (RAM)
+    while (dst < &_edata)
+    {
+        *dst++ = *src++; // Copy word and increment pointers
+    }
+
+    // Initialize .bss section (zero out)
+    dst = &_sbss;
+    while (dst < &_ebss)
+    {
+        *dst++ = 0; // Zero word and increment pointer
+    }
 
     xReturn = xTaskCreate(vTask1, "T1", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
     xReturn = xTaskCreate(vTask2, "T2", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
